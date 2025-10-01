e75311e65750460d284455dd22b5f86252d72b6e
holywc <chinweicong06@gmail.com>
2025-10-01 15:41:35 +0800

    update: added quarter logic + iPad-friendly buttons

--- app_kivy.py
+++ app_kivy.py
@@ -96,7 +96,7 @@ if st.session_state.pending_action:
     player, action, act_time = st.session_state.pending_action
     
     # Show prominent alert at the top
-    st.image("shot_chart.png", caption="Local Image", use_column_width=True)
+    st.image("shot_chart.png", caption="Local Image")
     st.stop()
 
 # --- Bench UI ---
